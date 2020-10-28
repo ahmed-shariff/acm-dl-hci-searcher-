61d84d75b6ab87c6f66e396acd8492022b3f1155
Ahmed Shariff <shariff.mfa@outlook.com>
2020-10-28 16:33:47 -0500

    Saving global information and amended cli for more functionality

--- acm_dl_searcher/__main__.py
+++ acm_dl_searcher/__main__.py
@@ -11,17 +11,23 @@ DATA_DIRECTORY = Path.home() / ".acm_dl_data"
 SEARCH_STRING = "https://dl.acm.org/action/doSearch?LimitedContentGroupKey={key}&pageSize=50&startPage={page_id}"
 
 
-def process_venue_data_from_doi(doi, overwrite=False):
+# TODO: better logging
+# TODO: parallelize data collection
+def _process_venue_data_from_doi(doi, short_name=None, overwrite=False):
     """
     Takes a doi of a venue in acm and get the entries and abstract. The data will be cacheed in DATA_DIRECTORY. 
     Returns a dictionary containing the doi as keys and the details as values
     """    
     doi_file = _ensure_data_directory_exists() / (doi.replace("/", "_") + ".json")
+    doi_title = bibtexparser.loads(requests.get(f"http://doi.org/{doi}", headers={"Accept": "application/x-bibtex"}).text).entries[0]["title"]
+    print(f"Title: {doi_title}")
+    _update_collection_info(doi_file.name, doi, doi_title, short_name)
+    
     if not overwrite and doi_file.exists():
         with open(doi_file) as f:
             doi_list_details = json.load(f)
         return doi_list_details
-
+    
     doi = doi.replace("/", "%2F")
     search_string = SEARCH_STRING.format(key=doi, page_id=0)
     response = requests.get(search_string)
@@ -63,3 +69,22 @@ def _ensure_data_directory_exists():
         DATA_DIRECTORY.mkdir()
     return DATA_DIRECTORY
     
+
+def _get_collection_info():
+    info_file = _ensure_data_directory_exists() / "info.json"
+    if info_file.exists():
+        with open(info_file) as f:
+            info = json.load(f)
+    else:
+        info = {}
+    return info, info_file
+        
+
+def _update_collection_info(file_name, doi, title, short_name):
+    info, info_file = _get_collection_info()
+
+    info[file_name] = {"doi": doi, "title": title, "short_name": short_name}
+
+    with open(info_file, "w") as f:
+        json.dump(info, f)
+
--- acm_dl_searcher/cli.py
+++ acm_dl_searcher/cli.py
@@ -2,16 +2,26 @@
 import sys
 
 import click
-from acm_dl_searcher.__main__ import process_venue_data_from_doi
+from acm_dl_searcher.__main__ import (_process_venue_data_from_doi,
+                                      _get_collection_info)
 
-@click.command()
-def main():
+@click.group()
+def cli():
     """Console script for acm_dl_hci_searcher."""
+    pass
+
+@cli.command()
+def get():
+    """Get the information for an entry"""
     doi = "10.1145/3313831"
     # TODO: Setting True for testing
-    print(process_venue_data_from_doi(doi, True))
-    
+    _process_venue_data_from_doi(doi, "CHI 20")
 
+
+@cli.command()
+def list():
+    """List all the details"""
+    print(*(_get_collection_info()), sep="\n")
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
