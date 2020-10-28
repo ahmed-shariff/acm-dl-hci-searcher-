865f0953c31f94bf5d89464c2dc631564cf5c866
Ahmed Shariff <ahmed.shariff@live.com>
2020-10-28 00:29:13 -0500

    Initial version for getting publication details from a doi of venu

--- acm_dl_searcher/__main__.py
+++ acm_dl_searcher/__main__.py
@@ -1,14 +1,54 @@
 """Main module."""
 import requests
 import re
+from bs4 import BeautifulSoup
+import bibtexparser
+from pathlib import Path
+import json
+from tqdm import tqdm
 
-def process_doi():
-    search_string = "https://dl.acm.org/action/doSearch?LimitedContentGroupKey={key}&pageSize=20&startPage=1"
-    doi = "10.1145/3313831"
+DATA_DIRECTORY = Path.home() / ".acm_dl_data"
+SEARCH_STRING = "https://dl.acm.org/action/doSearch?LimitedContentGroupKey={key}&pageSize=20&startPage=0"
+
+
+def process_venue_data_from_doi(doi, overwrite=False):
+    """
+    Takes a doi of a venue in acm and get the entries and abstract. The data will be cacheed in DATA_DIRECTORY. 
+    Returns a dictionary containing the doi as keys and the details as values
+    """    
+    doi_file = _ensure_data_directory_exists() / (doi.replace("/", "_") + ".json")
+    if not overwrite and doi_file.exists():
+        with open(doi_file) as f:
+            doi_list_details = json.load(f)
+        return doi_list_details
+
     doi = doi.replace("/", "%2F")
-    search_string = search_string.format(key=doi)
+    search_string = SEARCH_STRING.format(key=doi)
     response = requests.get(search_string)
-    re.compile(r"https://doi.org/10.1145/3313831.3376868")
-    print(response.text)
-    
+    with open("temp.html", "w") as f:
+        f.write(response.text)
+
+    doi_matcher = re.compile(r">https:\/\/doi\.org\/\d{2}\.\d{4}\/\d+\.\d+<")
+    # A working curl for doi meta data: curl --location --silent --header "Accept: application/x-bibtex" https://doi.org/10.1145/3313831.3376868
+    doi_list = [i.rstrip("<").lstrip(">") for i in doi_matcher.findall(response.text)]
+
+    doi_list_details = []
+    for doi_url in tqdm(doi_list):
+        details = bibtexparser.loads(requests.get(doi_url, headers={"Accept": "application/x-bibtex"}).text).entries[0]
+        abstract_html = requests.get("https://dl.acm.org/doi/" + details["doi"]).text
+        soup = BeautifulSoup(abstract_html, features="html.parser")
+        abstract = soup.findAll("div", {"class": "abstractInFull"})[0].find("p").get_text()
+        details["abstract"] = abstract
+        doi_list_details.append(details)
+
+    with open(doi_file, "w") as f:
+        json.dump(doi_list_details, f)
+
+    return doi_list_details
+
+
+def _ensure_data_directory_exists():
+    if not DATA_DIRECTORY.exists():
+        DATA_DIRECTORY.mkdir()
+    return DATA_DIRECTORY
     
--- acm_dl_searcher/cli.py
+++ acm_dl_searcher/cli.py
@@ -2,12 +2,15 @@
 import sys
 
 import click
-from acm_dl_searcher.__main__ import process_doi
+from acm_dl_searcher.__main__ import process_venue_data_from_doi
 
 @click.command()
 def main():
     """Console script for acm_dl_hci_searcher."""
-    process_doi()
+    doi = "10.1145/3313831"
+    # TODO: Setting True for testing
+    print(process_venue_data_from_doi(doi, True))
+    
 
 
 if __name__ == "__main__":
